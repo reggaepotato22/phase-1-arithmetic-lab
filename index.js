14478b2fde1ec6d419d93280268130251de2710f
Andrew <andrewmandieka@gmail.com>
2025-03-09 12:52:08 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;  // 31 * 2 = 62
+
+const random = Math.floor(Math.random() * 100) + 1;  // Generates a random integer > 0
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;  // 16 % 6 = 4
+
+const max = Math.max(1, 5, 10, 20);  // Returns 20 as the highest number
